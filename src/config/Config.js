5d0b914aeb77d7018f21fc50713c5a04a6658a03
antounious22 <antoinious.alchahda@studytafensw.edu.au>
2023-12-13 16:00:41 +1100

    Initial commit

---
+++ src/config/Config.js
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+  apiKey: "AIzaSyAApxC2sLg1x70ZVYotCtrvb9zd0GC0AqA",
+  authDomain: "moviefans-e00e4.firebaseapp.com",
+  projectId: "moviefans-e00e4",
+  storageBucket: "moviefans-e00e4.appspot.com",
+  messagingSenderId: "443567887178",
+  appId: "1:443567887178:web:7f5ab9d110a2896fa2e7b3"
+}
